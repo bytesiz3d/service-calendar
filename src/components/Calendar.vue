5da319fd7aafc43fb3cb0910aafdf89a2af4f792
bytesiz3d <abdelrahman.farid@gmail.com>
2023-09-08 10:36:00 +0300

    Fix carousel scrolling

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,29 +3,30 @@ import CalendarMonth from "@/components/CalendarMonth.vue";
 import { FirstDayOfEachMonthGenerator, DATES, Day } from "@/common";
 import { computed, onMounted } from "vue";
 import { useSearchDateStore, useStartDateStore } from "@/store";
-import { Carousel } from "bootstrap";
 import { storeToRefs } from "pinia";
 
 const { useCarousel } = defineProps<{ useCarousel: boolean }>();
+
 const searchStore = useSearchDateStore();
+const { searchDate: searchRef } = storeToRefs(searchStore);
+const searchDate = computed(() => new Day(searchRef.value));
 
 const startStore = useStartDateStore();
 const { startDate: startRef } = storeToRefs(startStore);
 const startDate = computed(() => new Day(startRef.value));
 
 function scrollToDate(dateString: string) {
-  if (useCarousel)
-  {
-    const carouselElement = document.querySelector('#monthCarousel');
-    const carousel = new Carousel(carouselElement ?? "");
-    carousel.to(new Date(dateString).getMonth() - new Date(startDate.value).getMonth());
-  }
-  else
-  {
-    const day_element = document.getElementById(dateString);
-    day_element?.scrollIntoView({ block: "center", inline: "nearest" });
-  }
+  const day_element = document.getElementById(dateString);
+  day_element?.scrollIntoView({ block: "center", inline: "nearest" });
 }
+
+const scrollToCarouselIndex = computed(() => {
+  const gen = new FirstDayOfEachMonthGenerator(startDate.value, DATES.end);
+
+  return [...gen.gen()]
+    .findIndex((firstDay: Day) => firstDay.getFullYear() == searchDate.value.getFullYear()
+      && firstDay.getMonth() == searchDate.value.getMonth());
+});
 
 onMounted(() => scrollToDate(searchStore.searchDate));
 searchStore.$subscribe((_, { searchDate }) => scrollToDate(searchDate));
@@ -38,22 +39,14 @@ const firstDayOfEachMonth = new FirstDayOfEachMonthGenerator(
 
 <template>
   <div class="container" v-if="useCarousel == false">
-    <CalendarMonth
-      v-for="day in firstDayOfEachMonth.gen()"
-      class="my-5"
-      :first-day-of-month="day"
-    />
+    <CalendarMonth v-for="day in firstDayOfEachMonth.gen()" class="my-5" :first-day-of-month="day" />
   </div>
 
   <div id="monthCarousel" class="carousel carousel-dark slide container" v-else>
     <div class="carousel-inner">
-      <CalendarMonth
-        v-for="(day, index) in firstDayOfEachMonth.gen()"
-        class="carousel-item my-5"
-        :class="{ active: index == 0 }"
-        style="padding-left: 10%; padding-right: 10%;"
-        :first-day-of-month="day"
-      />
+      <CalendarMonth v-for="(day, index) in firstDayOfEachMonth.gen()" class="carousel-item my-5"
+        :class="{ active: index == scrollToCarouselIndex }" style="padding-left: 10%; padding-right: 10%;"
+        :first-day-of-month="day" />
     </div>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#monthCarousel" data-bs-slide="prev">
